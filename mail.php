aba60a0893be512273e8623679bd7db57d74d77b
devjoaovictor <devjoao-teste@hotmail.com>
2022-09-07 15:10:13 -0300

    Primeiro Commit

--- mail.php
+++ mail.php
@@ -1,26 +1,31 @@
-//Contact Form in PHP
-<?php
-  $name = htmlspecialchars($_POST['name']);
-  $email = htmlspecialchars($_POST['email']);
-  $phone = htmlspecialchars($_POST['phone']);
-  $website = htmlspecialchars($_POST['website']);
-  $message = htmlspecialchars($_POST['message']);
+<?php 
 
-  if(!empty($email) && !empty($message)){
-    if(filter_var($email, FILTER_VALIDATE_EMAIL)){
-      $receiver = "contato@risaengenharia.com.br"; //enter that email address where you want to receive all messages
-      $subject = "From: $name <$email>";
-      $body = "Name: $name\nEmail: $email\nPhone: $phone\nWebsite: $website\n\nMessage:\n$message\n\nRegards,\n$name";
-      $sender = "From: $email";
-      if(mail($receiver, $subject, $body, $sender)){
-         echo "Your message has been sent";
-      }else{
-         echo "Sorry, failed to send your message!";
-      }
-    }else{
-      echo "Enter a valid email address!";
-    }
-  }else{
-    echo "Email and message field is required!";
-  }
+
+if(isset($_POST['email']) && !empty($_POST['email'])) {
+
+$nome = addslashes($_POST['name']);
+$email = addslashes($_POST['email']);
+$mensagem = addslashes($_POST['mensagem']);
+
+
+$to = "contato@risaengenharia.com.br";
+$subject = "Contato - teste";
+$body = "Nome: " .$nome. "\n".
+        "Email: " .$email. "\n".
+        "mensagem: " .$mensagem;
+$header = "From: teste@example.com.br"."\r\n".
+            "Reply-to:".$email."\e\n".
+            "X=Mailer:PHP/".phpversion();
+
+if(mail($to,$subject,$body,$header)) {
+
+    echo("Email enviado com sucesso!");
+
+}else {
+    echo("O Email não pode ser enviado!");
+}
+
+
+}
+
 ?>
